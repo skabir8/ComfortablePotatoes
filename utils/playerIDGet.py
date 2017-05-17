a0ec443f22c1d808c8177d8d12fb147db0de39ba
Vincent Liok <vliok@stuy.edu>
2017-05-17 14:29:42 -0400

    Scraped all Ids of the players

---
+++ utils/playerIDGet.py
@@ -0,0 +1,27 @@
+import requests
+import urllib2 
+
+# Headers required to receive NBA response
+headers1 = {'user-agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) '
+                          'AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/45.0.2454.101 Safari/537.36'),
+                     'referer': 'http://stats.nba.com/scores/'
+                    }
+
+
+
+r = requests.get("http://stats.nba.com/stats/leagueLeaders?LeagueID=00&PerMode=PerGame&Scope=S&Season=2016-17&SeasonType=Playoffs&StatCategory=PTS", headers = headers1)
+data = r.json()
+
+#seperates data in playerID rows
+playerID=data['resultSet']['rowSet']
+playerDict={}
+
+#makes a dict with ID being key and value as name
+for x in playerID:
+    playerDict[str(x[0])]= x[2]
+
+print playerDict
+
+    
+
