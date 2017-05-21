fbf3433f161328098974eea12fcfbc639b6a6793
Issac <ikim1@stuy.edu>
2017-05-21 13:56:13 -0400

    try to start work

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/newLeague')
+def newLeague():
+    return render_template('newLeague.html')
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
