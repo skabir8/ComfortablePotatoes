acc2a63e8cee7d414f1ca4daa6756a4edde89f55
Issac Kim <ikim1@stuy.edu>
2017-05-30 13:21:28 -0400

    trying to  pull

--- static/playerInfo.js
+++ static/playerInfo.js
@@ -9,3 +9,9 @@ for(var i=0; i<playerButtons.length;i++){
     var b = playerButtons[i];
     b.addEventListener("click", addPlayer);
 }
+
+var statButtons = document.getElementsByClassName("displayStats");
+for(var i=0; i<statButtons.length; i++){
+    var b = statButtons[i];
+    b.addEventListener("click", 
+}
